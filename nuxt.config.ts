ef023f45e277d8744b4b15f2731ad0fc65ca98b8
Aaron Pierson <me@aaronpierson.me>
2023-08-24 16:52:40 -0500

    adding packs for images darkmode anim icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
-  })
+  modules: ['@nuxt/content', '@nuxtjs/color-mode', 'nuxt-icon', '@nuxt/image'],
+  app: {
+    // pageTransition: { name: 'page', mode: 'out-in' }
+  },
+})
