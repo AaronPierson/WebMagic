ea931b972df3338106b422e62d815c1e31e23057
Aaron Pierson <me@aaronpierson.me>
2023-08-27 14:56:25 -0500

    editing layout

--- pages/lore/index.vue
+++ pages/lore/index.vue
@@ -2,25 +2,27 @@
     <div>
       <h1>Lore</h1>
       <p>Here is where you can find the lore of the world. Books collected over the years.</p>
-  
-      <!-- Magnus book of lore -->
-      <div class="chapter">
-        <h2 class="chapter-title"><NuxtLink to="/lore/magnus">Magnus</NuxtLink></h2>
-        <p class="chapter-description">
-          Behold, a scholarly tapestry woven through ages, where knowledge is the guiding star and code is the ancient language. In Magnus's Chronicles, delve into the meticulous accounts of web development's history, its evolution, and the secrets buried deep within its virtual realm.
-        </p>
+      <div class="books-grid">
+        <!-- Magnus book of lore -->
+        <div class="chapter">
+          <h2 class="chapter-title"><NuxtLink to="/lore/magnus">Magnus</NuxtLink></h2>
+          <p class="chapter-description">
+            Behold, a scholarly tapestry woven through ages, where knowledge is the guiding star and code is the ancient language. In Magnus's Chronicles, delve into the meticulous accounts of web development's history, its evolution, and the secrets buried deep within its virtual realm.
+          </p>
+        </div>
+        
+        <!-- Raelin book of lore -->
+        <div class="chapter">
+          <h2 class="chapter-title"><NuxtLink to="/lore/raelin">Raelin</NuxtLink></h2>
+          <p class="chapter-description">
+            Join Raelin on an epic journey where the web is a canvas, and developers, modern bards. In Raelin's Ballads, stories of heroics are spun from code, and the digital world comes alive with creativity. These are tales of the web's magic, where innovation and artistry meet in a harmonious symphony.
+          </p>
+        </div>
       </div>
-  
-      <!-- Raelin book of lore -->
-      <div class="chapter">
-        <h2 class="chapter-title"><NuxtLink to="/lore/raelin">Raelin</NuxtLink></h2>
-        <p class="chapter-description">
-          Join Raelin on an epic journey where the web is a canvas, and developers, modern bards. In Raelin's Ballads, stories of heroics are spun from code, and the digital world comes alive with creativity. These are tales of the web's magic, where innovation and artistry meet in a harmonious symphony.
-        </p>
-      </div>
+
     </div>
   </template>
-  
+
   <style scoped>
     .chapter {
       background: #f6ead4; /* Parchment-like background color */
@@ -43,6 +45,15 @@
       font-size: 18px;
       line-height: 1.4;
       margin: 0;
+    }
+
+    .books-grid {
+      /* Use CSS Grid to control the layout of the books */
+      display: grid;
+      /* 300px minmax ensures that the grid items are not squished too small */
+      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+      /* 20px grid gap to add space between the books */
+      grid-gap: 20px;
     }
 </style>
   
