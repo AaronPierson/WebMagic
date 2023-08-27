5afc81ef8ad593d5f9179230aa3a33b13a19e0db
Aaron Pierson <me@aaronpierson.me>
2023-08-27 14:55:07 -0500

    style and layout for jobs and projects cards

---
+++ components/Jobs.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <!-- <h3>Occupational Guilds</h3> -->
+        <div class="">
+        <!-- <transition-group name="fade" tag="ul" appear> -->
+        <ul class="portfolio-grid">
+            <li v-for="(job, index) in jobs" :key="index">
+                <div class="portfolio-card">
+                <nuxt-img :src="job.image" alt="Job Image" sizes="sm:30vw md:50vw lg:300px" quality="75"/>
+                <h3>{{ job.title }}</h3>
+                <p>{{ job.description }}</p>
+                <a v-if="job.link" :href="job.link" target="_blank" rel="noopener">Visit Site</a>
+                </div>
+            </li>
+        </ul>
+        <!-- </transition-group> -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        {
+          title: 'Sustalytics',
+          description: 'Description of Job 1',
+          image: '/img/OIP.jpg',
+          link: 'https://sustalytics.com/',
+        },
+        {
+          title: 'Job 2',
+          description: 'Description of Job 2',
+          image: '/images/job2.jpg',
+        },
+        // Add more jobs
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped src="@/assets/achievements.css">
+
+</style>
---
+++ components/Projects.vue
@@ -0,0 +1,53 @@
+<template>
+    <!-- <div class="portfolio-grid"> -->
+    <!-- <h3>Adventurous Projects</h3> -->
+    <div class="">
+        <!-- <transition-group name="fade" tag="ul"> -->
+        <ul class="portfolio-grid">
+            <li v-for="(project, index) in projects" :key="index">
+                <div class="portfolio-card">
+                    <nuxt-img :src="project.image" alt="Project Image" sizes="sm:100vw md:50vw lg:400px" />
+                    <h3>{{ project.title }}</h3>
+                    <p>{{ project.description }}</p>
+                </div>
+            </li>
+        </ul>
+        <!-- </transition-group> -->
+    </div>
+</template>
+
+ <script>
+  export default {
+    data() {
+      return {
+        projects: [
+          {
+            title: 'Project 1',
+            description: 'Description of Project 1',
+            image: '/images/project1.jpg', // Replace with the path to your image
+          },
+          {
+            title: 'Project 2',
+            description: 'Description of Project 2',
+            image: '/images/project2.jpg',
+          },
+          {
+            title: 'Project 2',
+            description: 'Description of Project 2',
+            image: '/images/project2.jpg',
+          },
+          {
+            title: 'Project 2',
+            description: 'Description of Project 2',
+            image: '/images/project2.jpg',
+          },
+          // Add more projects
+        ],
+       
+      };
+    },
+  };
+</script>
+<style scoped src="@/assets/achievements.css">
+
+</style>
