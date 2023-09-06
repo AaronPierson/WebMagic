0ae80d93edc6428b8aa6a98ea1349f575b73900d
Aaron Pierson <me@aaronpierson.me>
2023-09-06 13:09:16 -0500

    blog cards

--- components/Blog.vue
+++ components/Blog.vue
@@ -1,6 +1,21 @@
 <script setup>
-const { data } = await useAsyncData('blog', () => queryContent('blog').only(['title', 'date', 'image', 'tags', 'excerpt']).find())
+  import { defineProps } from 'vue';
+  // import { calculateReadTime } from '~/assets/readTime'; // Import your read time utility
+  // const { data } = await useAsyncData('blog', () => queryContent('blog').only(['title', 'date', 'image', 'tags', 'excerpt', '_path']).find());
+  // const { data } = await useAsyncData('blog', () => queryContent('blog').find());
+  // Define props within the same script block
+  const props = defineProps({
+    limit: Number,
+  });
+
+  const { data } = await useAsyncData('blog', () => queryContent('blog').only(['title', 'date', 'image', 'tags', 'excerpt', '_path']).limit(props.limit).find());
+  // Calculate read time for each blog post
+  // const blogsWithReadTime = data.map((blog) => ({
+  //   ...blog,
+  //   readTime: calculateReadTime(blog.content), // Assuming content contains your blog post text
+  // }));
 </script>
+
 
 <template>
   <main>
@@ -10,6 +25,8 @@ const { data } = await useAsyncData('blog', () => queryContent('blog').only(['ti
         <h3>{{ blog.title }}</h3>
         <p>{{ blog.excerpt }}</p>
         <p>{{ blog.date }}</p>
+        <!-- <p>Estimated read time: {{ readTime }} minute(s)</p> -->
+        <NuxtLink :to="blog._path">Read More</NuxtLink>
         <div v-for="(tag, tagIndex) in blog.tags" :key="tagIndex" class="tag">
           {{ tag }}
         </div>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,4 +1,3 @@
 <template>
-
     <Blog />
 </template>
