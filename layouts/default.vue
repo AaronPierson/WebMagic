ec917b9315a1ffb117115403b6dde2c32fd1c2bc
Aaron Pierson <me@aaronpierson.me>
2023-10-11 16:47:04 -0500

    moblie ham menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,23 +6,55 @@
   </head>
 
   <header class="navbar">
-        <nav class="nav-container">
-                  <!-- <h1>Color mode: {{ $colorMode.value }}</h1> -->
+
+    <!-- Desktop Navigation Bar -->
+    <div class="desktop-navbar">
+      <nav class="nav-container">
         <select v-model="$colorMode.preference"  >
           <option value="system">System</option>
           <option value="light">Light</option>
           <option value="dark">Dark</option>
           <!-- <option value="sepia">Sepia</option> -->
         </select>
-      <!-- Burger menu -->
-      <button class="style-module--Burger--1ab74" @click="toggleSidebar">
-          <!-- Add your burger icon elements here -->
-          <span>---</span>
-          <span>---</span>
-          <span>---</span>
+        <div class="logo">
+          <NuxtLink to="/"></NuxtLink>
+        </div>
+        <ul class="nav-menu">
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/lore">Lore</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/blog">Adventure Log</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/achievements">Scroll of Achievements</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
 
-      </button>
-        <div class="style-module--SideBarWrapper--e7152" :class="{ 'open': sidebarOpen }">>
+    <!-- Mobile Sidebar Navigation Bar (as you already have) -->
+    <div class="navbar-mobile">
+        <nav class="nav-container">
+        <!-- <h1>Color mode: {{ $colorMode.value }}</h1> -->
+        <!-- Sidebar container -->
+        <div class="nav-sidebar" :class="{ 'open': sidebarOpen }">
+          <select v-model="$colorMode.preference"  >
+          <option value="system">System</option>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+          <!-- <option value="sepia">Sepia</option> -->
+        </select>
+          <!-- Close button for mobile -->
+          <!-- <button class="close-menu-button" @click="closeSidebar"> -->
+            <!-- Add your close button icon elements here -->
+            <!-- <span>&times;</span>
+            <span>Close</span>
+
+          </button> -->
           <div class="logo">
             <NuxtLink to="/"></NuxtLink>
           </div>
@@ -42,12 +74,17 @@
           </ul>
         </div>
         <!-- Sidebar backdrop -->
-        <div class="style-module--SideBarBackdrop--fb470" @click="closeSidebar" :class="{ 'open': sidebarOpen }">
+        <div class="SideBarBackdrop" @click="closeSidebar" :class="{ 'open': sidebarOpen }">
         </div>
-
+        <!-- Burger menu -->
+        <button class="menu-button" @click="toggleSidebar" :class="{ active: sidebarOpen }">
+            <!-- Add your burger icon elements here -->
+            <div class="ham-line"></div>
+            <div class="ham-line"></div>
+            <div class="ham-line"></div>
+        </button>
         </nav>
-        <div>
-      </div>
+    </div>
   </header>
   <main class="parchment-background">
     <slot />
@@ -168,6 +205,34 @@ body {
   box-sizing: border-box; /* Include the border in the element's dimensions */
 }
 
+
+/* Add these styles to your existing styles */
+.menu-button {
+  /* Your existing styles for the button */
+  cursor: pointer;
+}
+
+.ham-line {
+  width: 30px;
+  height: 3px;
+  background-color: #000; /* Color of the lines */
+  margin: 6px 0;
+  transition: 0.4s; /* Animation duration and easing */
+}
+
+/* Styles for the animated "X" shape when the menu is open */
+.menu-button.active .ham-line:nth-child(1) {
+  transform: rotate(-45deg) translate(-5px, 6px);
+}
+
+.menu-button.active .ham-line:nth-child(2) {
+  opacity: 0;
+}
+
+.menu-button.active .ham-line:nth-child(3) {
+  transform: rotate(45deg) translate(-5px, -6px);
+}
+
 /* Add styles for color modes */
 .dark-mode body{
   background-color: #091a28;
@@ -195,24 +260,53 @@ body {
 }
 
 /* Sidebar Container */
-.style-module--SideBarWrapper--e7152 {
+.nav-sidebar {
   position: fixed;
   top: 0;
   right: -300px; /* Adjust the initial position */
   width: 300px; /* Adjust the sidebar width */
   height: 100%;
-  background-color: #fff; /* Set the background color of the sidebar */
+  background-color: #f1e7d0; /* Set the background color of the sidebar */
+  transition: right 0.3s ease; /* Add a smooth transition for opening and closing */
+}
+
+/* Sidebar Container */
+.dark-mode .nav-sidebar {
+  position: fixed;
+  top: 0;
+  right: -300px; /* Adjust the initial position */
+  width: 300px; /* Adjust the sidebar width */
+  height: 100%;
+  background-color: #433422; /* Set the background color of the sidebar */
   transition: right 0.3s ease; /* Add a smooth transition for opening and closing */
 }
 
 /* Sidebar Opened State */
-.style-module--SideBarWrapper--e7152.open {
+.nav-sidebar.open {
   right: 0; /* When open, the sidebar should be fully visible */
+}
+
+.close-menu-button{
+  /* center the button */
+  margin: 0 auto;
+  display: block;
+  padding: 10px;
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  font-size: 48px;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: black;
+  transition: color 0.3s ease;
+  line-height: 48px; /* Adjust the line-height to vertically center the text */
+
 }
 
 /*
 Sidebar Backdrop
-.style-module--SideBarBackdrop--fb470 {
+.SideBarBackdrop {
   position: fixed;
   top: 0;
   right: 0; 
@@ -230,29 +324,64 @@ Sidebar Backdrop
 /* } */
 
 /* Sidebar Opened State (Backdrop) */
-.style-module--SideBarBackdrop--fb470.open {
+.SideBarBackdrop .open {
   opacity: 1; /* When the sidebar is open, make the backdrop fully visible */
 }
 
 
 /* Sidebar Navigation Links */
-.style-module--SideBarWrapper--e7152 .nav-menu {
+.nav-sidebar .nav-menu {
+  /*make link center vertically*/
   display: flex;
   flex-direction: column; /* Arrange links vertically */
+  align-items: center; /* Center the links vertically */
+  margin: 50% 0;
+
 }
 
-.style-module--SideBarWrapper--e7152 .nav-menu a {
+.nav-sidebar .nav-menu a {
   display: block;
   padding: 10px 0; /* Adjust the padding to control the spacing between items */
   text-align: center; /* Align text to the left within each block */
+  margin: 5px 0; /* Add margin to space the links vertically */
+
 }
 
 /* Burger Menu Icon */
-.style-module--Burger--1ab74 {
+.menu-button {
   cursor: pointer;
   padding: 10px;
 }
 
+/* For screens with a maximum width of 768px, show the sidebar */
+@media screen and (max-width: 768px) {
+  /* Sidebar styles */
+  .navbar-mobile {
+    display: block; /* Show the sidebar */
+  }
+
+  .nav-container{
+    margin: 0px 50px 0 50px;
+  }
+
+  body{
+    margin: 0px;
+  }
+
+  /* Hide the horizontal navigation menu */
+  .desktop-navbar  {
+    display: none;
+  }
+}
 
 
+  /* Show it on screens wider than a certain width (adjust max-width as needed) */
+  @media screen and (min-width: 769px) {
+    .desktop-navbar {
+      display: block;
+    }
+    .navbar-mobile {
+      display: none;
+    }
+  }
 </style>
