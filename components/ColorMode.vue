89e0e28c8f2d09692c4360ca5f04448c322eea5e
Aaron Pierson <me@aaronpierson.me>
2023-10-14 17:53:14 -0500

    making a color mode toggle

---
+++ components/ColorMode.vue
@@ -0,0 +1,9 @@
+<template>
+    <!-- <h1>Color mode: {{ $colorMode.value }}</h1> -->
+    <label class="switch">
+        <input type="checkbox" v-model="$colorMode.preference" value="dark" />
+        <span class="slider round"></span>
+    </label>
+</template>
+
+<style src="@assets/css/ColorMode.css"/>
